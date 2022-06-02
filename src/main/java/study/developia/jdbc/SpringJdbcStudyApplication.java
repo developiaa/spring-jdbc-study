ba74eed47f60338e3c5977ee217c0903fa485389
developiaa <developiaa@gmail.com>
2022-06-02 20:59:28 +0900

    first commit

---
+++ src/main/java/study/developia/jdbc/SpringJdbcStudyApplication.java
@@ -0,0 +1,13 @@
+package study.developia.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcStudyApplication.class, args);
+    }
+
+}
