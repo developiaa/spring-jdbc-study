02da126136e2e915617fb290ca6ec404858f5329
developiaa <developiaa@gmail.com>
2022-06-05 22:55:06 +0900

    feat : Add jdbc test(update, delete)

--- src/test/java/study/developia/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/study/developia/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,14 +1,13 @@
 package study.developia.jdbc.repository;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import study.developia.jdbc.domain.Member;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -27,5 +26,15 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}", member.equals(findMember));
         assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
