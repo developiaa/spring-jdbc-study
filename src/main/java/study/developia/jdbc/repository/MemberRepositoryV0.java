7df9f510b71bdfc210da1736efe5ce467c8d9368
developiaa <developiaa@gmail.com>
2022-06-05 22:41:24 +0900

    feat : Add jdbc test(select)

--- src/main/java/study/developia/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/study/developia/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import study.developia.jdbc.connection.DBConnectionUtil;
 import study.developia.jdbc.domain.Member;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -30,6 +31,35 @@ public class MemberRepositoryV0 {
             throw e;
         } finally {
             close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error : ", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
         }
     }
 
--- src/test/java/study/developia/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/study/developia/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,31 @@
 package study.developia.jdbc.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import study.developia.jdbc.domain.Member;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV1", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 }
